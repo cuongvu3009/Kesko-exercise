fc777aa4c5ba9d287d417945b7aea96d65d71d25
cuongvu3009 <cuongvu3009@gmail.com>
2023-03-16 17:44:31 +0200

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,62 @@
+import dotenv from 'dotenv';
+import bunyan from 'bunyan';
+import cloudinary from 'cloudinary';
+
+dotenv.config();
+
+class Config {
+  public DATABASE_URL: string | undefined;
+  public JWT_TOKEN: string | undefined;
+  public SECRET_KEY_ONE: string | undefined;
+  public SECRET_KEY_TWO: string | undefined;
+  public NODE_ENV: string | undefined;
+  public CLIENT_URL: string | undefined;
+  public SERVER_PORT: string | undefined;
+  public REDIS_HOST: string | undefined;
+  public CLOUDINARY_NAME: string | undefined;
+  public CLOUDINARY_API_KEY: string | undefined;
+  public CLOUDINARY_API_SECRET: string | undefined;
+
+  private readonly DEFAULT_DATABASE_URL =
+    'mongodb+srv://cuong:1234@nodeexpressproject.nlzou.mongodb.net/Chatty-App?retryWrites=true&w=majority';
+  private readonly DEFAULT_NODE_ENV = 'development';
+  private readonly DEFAULT_CLIENT_URL = 'http://localhost:3000';
+
+  constructor() {
+    this.DATABASE_URL = process.env.DATABASE_URl || this.DEFAULT_DATABASE_URL;
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '';
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
+    this.NODE_ENV = process.env.NODE_ENV || this.DEFAULT_NODE_ENV;
+    this.CLIENT_URL = process.env.CLIENT_URL || this.DEFAULT_CLIENT_URL;
+    this.SERVER_PORT = process.env.SERVER_PORT || '';
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
+    this.CLOUDINARY_NAME = process.env.CLOUDINARY_NAME || '';
+    this.CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || '';
+    this.CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || '';
+  }
+
+  //	logger
+  public createLogger(name: string): bunyan {
+    return bunyan.createLogger({ name, level: 'debug' });
+  }
+
+  //	make sure all env variable in config exist
+  public validateConfig(): void {
+    for (const [key, value] of Object.entries(this)) {
+      if (value === undefined) {
+        throw new Error(`Configuration ${key} is undefined`);
+      }
+    }
+  }
+
+  public cloudinaryConfig(): void {
+    cloudinary.v2.config({
+      cloud_name: this.CLOUDINARY_NAME,
+      api_key: this.CLOUDINARY_API_KEY,
+      api_secret: this.CLOUDINARY_API_SECRET
+    });
+  }
+}
+
+export const config: Config = new Config();
